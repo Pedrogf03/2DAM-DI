6c04060895ac8c7f4c916f253b72660e3d0c30b1
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-06 09:25:00 +0100

    6 11 23

--- PruebaFX/src/App.java
+++ PruebaFX/src/App.java
@@ -1,5 +1,34 @@
-public class App {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+
+    VBox root = new VBox();
+    Label label = new Label("Hola mundo");
+    Label label2 = new Label("Hello World");
+
+    root.setAlignment(Pos.CENTER);
+
+    label.setStyle("-fx-font-size:30px; -fx-text-fill:red;");
+
+    root.getChildren().addAll(label);
+    root.getChildren().addAll(label2);
+
+    Scene scene = new Scene(root, 400, 300);
+    stage.setScene(scene);
+    stage.setTitle("Hello JFX!");
+
+    stage.show();
+
   }
 }
