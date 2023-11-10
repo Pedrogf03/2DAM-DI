9d94f240b1955a09defe478649601650318aacbf
Pedrogf03 <bleikerpro23@gmail.com>
2023-11-10 12:39:36 +0100

    10 11 23

--- Layout/src/Calculator.java
+++ Layout/src/Calculator.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
